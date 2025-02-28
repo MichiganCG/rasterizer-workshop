354bf4110486069c7de1679ad11b8ad4a83c51f5
130oclock <aodan13@att.net>
2025-02-28 00:06:19 -0500

    updated comments

--- library/quaternion.cpp
+++ library/quaternion.cpp
@@ -3,19 +3,10 @@
 
 #define epsilon 8E-7f
 
-/**
- * Creates an identity quaternion with no rotation.
- */
 Quaternion::Quaternion() : w(1.0f), x(0.0f), y(0.0f), z(0.0f) {}
 
-/**
- * Creates a quaternion with the specified components.
- */
 Quaternion::Quaternion(float w, float x, float y, float z) : w(w), x(x), y(y), z(z) {}
 
-/**
- * Creates a quaternion using the given axis-angle representation.
- */
 Quaternion::Quaternion(const Vec3 &axis, float angle) : Quaternion()
 {
     w = std::cos(angle * 0.5);
@@ -38,31 +29,6 @@ Quaternion &Quaternion::operator=(const Quaternion &other)
     return *this;
 }
 
-/**
- * Applies the quaternion to the vector (0, 0, 1).
- */
-Vec3 Quaternion::forward()
-{
-    return {
-        2 * (x * z + w * y),
-        2 * (y * z - w * x),
-        1 - (2 * (x * x + y * y))};
-}
-
-/**
- * Applies the quaternion to the vector (0, 1, 0).
- */
-Vec3 Quaternion::up()
-{
-    return {
-        2 * (x * y - w * z),
-        1 - (2 * (x * x + z * z)),
-        2 * (y * z + w * x)};
-}
-
-/**
- * Applies the quaternion to the vector (1, 0, 0).
- */
 Vec3 Quaternion::right()
 {
     return {
@@ -71,17 +37,27 @@ Vec3 Quaternion::right()
         2 * (x * z - w * y)};
 }
 
-/**
- * Returns the imaginary component, also known as the bivector.
- */
+Vec3 Quaternion::up()
+{
+    return {
+        2 * (x * y - w * z),
+        1 - (2 * (x * x + z * z)),
+        2 * (y * z + w * x)};
+}
+
+Vec3 Quaternion::forward()
+{
+    return {
+        2 * (x * z + w * y),
+        2 * (y * z - w * x),
+        1 - (2 * (x * x + y * y))};
+}
+
 Vec3 Quaternion::bivector()
 {
     return {x, y, z};
 }
 
-/**
- * Computes the axis-angle representation of this quaternion.
- */
 void Quaternion::axis_angle(Vec3 &axis_out, float &angle_out)
 {
     Vec3 biv = bivector();
@@ -91,18 +67,11 @@ void Quaternion::axis_angle(Vec3 &axis_out, float &angle_out)
     angle_out = 2 * std::atan2(s, w);
 }
 
-/**
- * Applies the axis-angle rotation to this quaternion.
- */
 Quaternion &Quaternion::rotate(const Vec3 &axis, float angle)
 {
     return (*this *= Quaternion(normalize(axis), angle));
 }
 
-/**
- * Mutiplication of two quaternions is associative, but no commutative.
- * A rotation by A followed by a rotation by B is equal to C = A * B.
- */
 Quaternion &Quaternion::operator*=(const Quaternion &rhs)
 {
     Quaternion quaternion;
@@ -113,9 +82,6 @@ Quaternion &Quaternion::operator*=(const Quaternion &rhs)
     return (*this = quaternion);
 }
 
-/**
- * Mutiplication by a scalar.
- */
 Quaternion &Quaternion::operator*=(float rhs)
 {
     w *= rhs;
@@ -146,9 +112,6 @@ float magnitude(const Quaternion &q)
     return std::sqrt(magnitude_squared(q));
 }
 
-/**
- * https://stackoverflow.com/questions/11667783/quaternion-and-normalization
- */
 Quaternion normalize(const Quaternion &q)
 {
     float qmagsq = magnitude_squared(q);
@@ -157,10 +120,6 @@ Quaternion normalize(const Quaternion &q)
     return q * (1.0f / std::sqrt(qmagsq));
 }
 
-/**
- * Returns the conjugate of the quaternion.
- * For rotation quaternions, this is equal to the inverse.
- */
 Quaternion conjugate(const Quaternion &q)
 {
     return {q.w, -q.x, -q.y, -q.z};
--- library/quaternion.hpp
+++ library/quaternion.hpp
@@ -11,21 +11,35 @@ class Quaternion
 public:
     float w, x, y, z;
 
+    // Creates an identity quaternion with no rotation
     Quaternion();
+    // Creates a quaternion with the specified components
     Quaternion(float w, float x, float y, float z);
+    // Creates a quaternion using the given axis-angle representation
     Quaternion(const Vec3 &axis, float angle);
+    // Copy ctor
     Quaternion(const Quaternion &other);
     Quaternion &operator=(const Quaternion &other);
 
-    Vec3 forward();
-    Vec3 up();
+    // Rotates (1, 0, 0)
     Vec3 right();
+    // Rotates (0, 1, 0)
+    Vec3 up();
+    // Rotates (0, 0, 1)
+    Vec3 forward();
+
+    // Returns the imaginary component (bivector)
     Vec3 bivector();
 
+    // Computes the axis-angle representation of this rotation
     void axis_angle(Vec3 &axis_out, float &angle_out);
-
+    // Applies the axis-angle representation to this rotation
     Quaternion &rotate(const Vec3 &axis, float angle);
 
+    /**
+     * Mutiplication of two quaternions is associative, but no commutative
+     * A rotation by A followed by a rotation by B is equal to C = A * B
+     */
     Quaternion &operator*=(const Quaternion &rhs);
     Quaternion &operator*=(float rhs);
 
@@ -39,5 +53,13 @@ float dot(const Quaternion &, const Quaternion &);
 float magnitude_squared(const Quaternion &);
 float magnitude(const Quaternion &);
 
+/**
+ * Implementation from https://stackoverflow.com/questions/11667783/quaternion-and-normalization
+ * @return A unit quaternion with the same rotation
+ */
 Quaternion normalize(const Quaternion &);
+
+/**
+ * @return The inverse of the rotation
+ */
 Quaternion conjugate(const Quaternion &);
