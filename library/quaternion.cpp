ae80820633ca8cec1d39fcc0a3e7189cce6972e3
130oclock <aodan13@att.net>
2025-02-27 21:34:05 -0500

    Updated quaternion class

--- library/quaternion.cpp
+++ library/quaternion.cpp
@@ -3,20 +3,38 @@
 
 #define epsilon 8E-7f
 
-Quaternion::Quaternion() : r(0.0f), i(0.0f), j(0.0f), k(1.0f) {}
+/**
+ * Creates an identity quaternion with no rotation.
+ */
+Quaternion::Quaternion() : w(1.0f), x(0.0f), y(0.0f), z(0.0f) {}
 
-Quaternion::Quaternion(float r, float i, float j, float k) : r(r), i(i), j(j), k(k) {}
+/**
+ * Creates a quaternion with the specified components.
+ */
+Quaternion::Quaternion(float w, float x, float y, float z) : w(w), x(x), y(y), z(z) {}
 
-Quaternion::Quaternion(const Quaternion &other) : r(other.r), i(other.i), j(other.j), k(other.k) {}
+/**
+ * Creates a quaternion using the given axis-angle representation.
+ */
+Quaternion::Quaternion(const Vec3 &axis, float angle) : Quaternion()
+{
+    w = std::cos(angle * 0.5);
+    float sin_half = std::sin(angle * 0.5);
+    x = axis.x * sin_half;
+    y = axis.y * sin_half;
+    z = axis.z * sin_half;
+}
+
+Quaternion::Quaternion(const Quaternion &other) : w(other.w), x(other.x), y(other.y), z(other.z) {}
 
 Quaternion &Quaternion::operator=(const Quaternion &other)
 {
     if (this == &other)
         return *this;
-    r = other.r;
-    i = other.i;
-    j = other.j;
-    k = other.k;
+    w = other.w;
+    x = other.x;
+    y = other.y;
+    z = other.z;
     return *this;
 }
 
@@ -26,9 +44,9 @@ Quaternion &Quaternion::operator=(const Quaternion &other)
 Vec3 Quaternion::forward()
 {
     return {
-        2 * (i * k + r * j),
-        2 * (j * k - r * i),
-        1 - (2 * (i * i + j * j))};
+        2 * (x * z + w * y),
+        2 * (y * z - w * x),
+        1 - (2 * (x * x + y * y))};
 }
 
 /**
@@ -37,20 +55,20 @@ Vec3 Quaternion::forward()
 Vec3 Quaternion::up()
 {
     return {
-        2 * (i * j - r * k),
-        1 - (2 * (i * i + k * k)),
-        2 * (j * k + r * i)};
+        2 * (x * y - w * z),
+        1 - (2 * (x * x + z * z)),
+        2 * (y * z + w * x)};
 }
 
 /**
  * Applies the quaternion to the vector (1, 0, 0).
  */
-Vec3 Quaternion::left()
+Vec3 Quaternion::right()
 {
     return {
-        1 - (2 * (j * j + k * k)),
-        2 * (i * j + r * k),
-        2 * (i * k - r * j)};
+        1 - (2 * (y * y + z * z)),
+        2 * (x * y + w * z),
+        2 * (x * z - w * y)};
 }
 
 /**
@@ -58,46 +76,64 @@ Vec3 Quaternion::left()
  */
 Vec3 Quaternion::bivector()
 {
-    return {i, j, k};
+    return {x, y, z};
 }
 
+/**
+ * Computes the axis-angle representation of this quaternion.
+ */
+void Quaternion::axis_angle(Vec3 &axis_out, float &angle_out)
+{
+    Vec3 biv = bivector();
+    float s = magnitude(biv);
+    axis_out = biv * (1.0f / s);
+
+    angle_out = 2 * std::atan2(s, w);
+}
+
+/**
+ * Applies the axis-angle rotation to this quaternion.
+ */
+Quaternion &Quaternion::rotate(const Vec3 &axis, float angle)
+{
+    return (*this *= Quaternion(normalize(axis), angle));
+}
+
+/**
+ * Mutiplication of two quaternions is associative, but no commutative.
+ * A rotation by A followed by a rotation by B is equal to C = A * B.
+ */
 Quaternion &Quaternion::operator*=(const Quaternion &rhs)
 {
     Quaternion quaternion;
-    quaternion.r = (r * rhs.r) - (i * rhs.i) - (j * rhs.j) - (k * rhs.k);
-    quaternion.i = (r * rhs.i) + (i * rhs.r) + (j * rhs.k) - (k * rhs.j);
-    quaternion.j = (r * rhs.j) - (i * rhs.k) + (j * rhs.r) + (k * rhs.i);
-    quaternion.k = (r * rhs.k) + (i * rhs.j) - (j * rhs.i) + (k * rhs.r);
-    return (*this = normalize(quaternion));
+    quaternion.w = (w * rhs.w) - (x * rhs.x) - (y * rhs.y) - (z * rhs.z);
+    quaternion.x = (w * rhs.x) + (x * rhs.w) - (y * rhs.z) + (z * rhs.y);
+    quaternion.y = (w * rhs.y) + (x * rhs.z) + (y * rhs.w) - (z * rhs.x);
+    quaternion.z = (w * rhs.z) - (x * rhs.y) + (y * rhs.x) + (z * rhs.w);
+    return (*this = quaternion);
 }
 
+/**
+ * Mutiplication by a scalar.
+ */
 Quaternion &Quaternion::operator*=(float rhs)
 {
-    r *= rhs;
-    i *= rhs;
-    j *= rhs;
-    k *= rhs;
-    return *this;
-}
-
-Quaternion &Quaternion::operator/=(float rhs)
-{
-    r /= rhs;
-    i /= rhs;
-    j /= rhs;
-    k /= rhs;
+    w *= rhs;
+    x *= rhs;
+    y *= rhs;
+    z *= rhs;
     return *this;
 }
 
 std::ostream &operator<<(std::ostream &os, const Quaternion &rhs)
 {
-    os << rhs.r << " " << rhs.i << " " << rhs.j << " " << rhs.k << std::endl;
+    os << rhs.w << " " << rhs.x << " " << rhs.y << " " << rhs.z << std::endl;
     return os;
 }
 
 float dot(const Quaternion &lhs, const Quaternion &rhs)
 {
-    return lhs.r * rhs.r + lhs.i * rhs.i + lhs.j * rhs.j + lhs.k * rhs.k;
+    return lhs.w * rhs.w + lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
 }
 
 float magnitude_squared(const Quaternion &q)
@@ -121,32 +157,11 @@ Quaternion normalize(const Quaternion &q)
     return q * (1.0f / std::sqrt(qmagsq));
 }
 
+/**
+ * Returns the conjugate of the quaternion.
+ * For rotation quaternions, this is equal to the inverse.
+ */
 Quaternion conjugate(const Quaternion &q)
 {
-    return {q.r, -q.i, -q.j, -q.k};
-}
-
-void Quaternion::get_axis_angle(Vec3 &axis, float &angle)
-{
-    Vec3 biv = bivector();
-    float s = magnitude(biv);
-    axis = biv / s;
-
-    angle = 2 * std::atan2(s, r);
-}
-
-Quaternion local_rotation(const Vec3 &axis, float angle)
-{
-    float half_angle = angle * 0.5;
-    return {
-        std::cos(half_angle),
-        axis.x * std::sin(half_angle),
-        axis.y * std::sin(half_angle),
-        axis.z * std::sin(half_angle)};
-}
-
-void Quaternion::rotate(const Vec3 &axis, float angle)
-{
-    Quaternion local = local_rotation(normalize(axis), angle);
-    *this *= local;
+    return {q.w, -q.x, -q.y, -q.z};
 }
--- library/quaternion.hpp
+++ library/quaternion.hpp
@@ -4,41 +4,40 @@
 
 #include <ostream>
 
+// https://danceswithcode.net/engineeringnotes/quaternions/quaternions.html
+
 class Quaternion
 {
 public:
-    float r, i, j, k;
+    float w, x, y, z;
 
     Quaternion();
-    Quaternion(float, float, float, float);
-    Quaternion(const Quaternion &);
-    Quaternion &operator=(const Quaternion &);
+    Quaternion(float w, float x, float y, float z);
+    Quaternion(const Vec3 &axis, float angle);
+    Quaternion(const Quaternion &other);
+    Quaternion &operator=(const Quaternion &other);
 
     Vec3 forward();
     Vec3 up();
-    Vec3 left();
+    Vec3 right();
     Vec3 bivector();
 
-    void get_axis_angle(Vec3 &, float &);
+    void axis_angle(Vec3 &axis_out, float &angle_out);
 
-    void rotate(const Vec3 &, float);
+    Quaternion &rotate(const Vec3 &axis, float angle);
 
-    Quaternion &operator*=(const Quaternion &);
-    Quaternion &operator*=(float);
-    Quaternion &operator/=(float);
+    Quaternion &operator*=(const Quaternion &rhs);
+    Quaternion &operator*=(float rhs);
 
-	friend std::ostream &operator<<(std::ostream &, const Quaternion &);
+    friend std::ostream &operator<<(std::ostream &, const Quaternion &);
 };
 
-inline Quaternion operator*(Quaternion lhs, const Quaternion &rhs) { return (lhs *= rhs); }
-inline Quaternion operator*(Quaternion lhs, float rhs) { return (lhs *= rhs); }
-inline Quaternion operator/(Quaternion lhs, float rhs) { return (lhs /= rhs); }
+Quaternion operator*(Quaternion lhs, Quaternion rhs) { return (lhs *= rhs); }
+Quaternion operator*(Quaternion lhs, float rhs) { return (lhs *= rhs); }
 
-inline float dot(const Quaternion &, const Quaternion &);
-inline float magnitude_squared(const Quaternion &);
-inline float magnitude(const Quaternion &);
+float dot(const Quaternion &, const Quaternion &);
+float magnitude_squared(const Quaternion &);
+float magnitude(const Quaternion &);
 
 Quaternion normalize(const Quaternion &);
 Quaternion conjugate(const Quaternion &);
-
-Quaternion local_rotation(const Vec3 &, float);
