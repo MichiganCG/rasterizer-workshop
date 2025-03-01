c85b997e9bc15ffe1b6c91c202925cb0a11a3120
130oclock <aodan13@att.net>
2025-03-01 02:52:28 -0500

    added plane intersect

--- library/vectors.cpp
+++ library/vectors.cpp
@@ -1,19 +1,9 @@
 #include "vectors.hpp"
 #include "library.hpp"
 
-Vec2 &Vec2::operator=(const Vec2 &other)
-{
-    if (this == &other)
-        return *this;
-    u = other.u;
-    v = other.v;
-    w = other.w;
-    return *this;
-}
-
-const Vec3 Vec3::RIGHT = Vec3(1, 0, 0);
-const Vec3 Vec3::UP = Vec3(0, 1, 0);
-const Vec3 Vec3::FORWARD = Vec3(0, 0, 1);
+const Vec3 Vec3::RIGHT = Vec3(1, 0, 0, 0);
+const Vec3 Vec3::UP = Vec3(0, 1, 0, 0);
+const Vec3 Vec3::FORWARD = Vec3(0, 0, 1, 0);
 
 Vec3 &Vec3::operator=(const Vec3 &other)
 {
@@ -31,65 +21,63 @@ Vec3 &Vec3::operator+=(const Vec3 &rhs)
     x += rhs.x;
     y += rhs.y;
     z += rhs.z;
-    w += rhs.w;
     return *this;
 }
-
-Vec3 operator+(Vec3 lhs, const Vec3 &rhs) { return (lhs += rhs); }
 
 Vec3 &Vec3::operator-=(const Vec3 &rhs)
 {
     x -= rhs.x;
     y -= rhs.y;
     z -= rhs.z;
-    w -= rhs.w;
     return *this;
 }
-
-Vec3 operator-(Vec3 lhs, const Vec3 &rhs) { return (lhs -= rhs); }
 
 Vec3 &Vec3::operator*=(const Vec3 &rhs)
 {
     x *= rhs.x;
     y *= rhs.y;
     z *= rhs.z;
-    w *= rhs.w;
     return *this;
 }
-
-Vec3 operator*(Vec3 lhs, const Vec3 &rhs) { return (lhs *= rhs); }
 
 Vec3 &Vec3::operator/=(const Vec3 &rhs)
 {
     x /= rhs.x;
     y /= rhs.y;
     z /= rhs.z;
-    w /= rhs.w;
     return *this;
 }
-
-Vec3 operator/(Vec3 lhs, const Vec3 &rhs) { return (lhs /= rhs); }
 
 Vec3 &Vec3::operator*=(float rhs)
 {
     x *= rhs;
     y *= rhs;
     z *= rhs;
-    w *= rhs;
     return *this;
 }
-
-Vec3 operator*(Vec3 lhs, float rhs) { return (lhs *= rhs); }
 
 Vec3 &Vec3::operator/=(float rhs)
 {
     x /= rhs;
     y /= rhs;
     z /= rhs;
-    w /= rhs;
     return *this;
 }
 
+Vec3::operator Vec2() const
+{
+    Vec2 v;
+    v.u = x;
+    v.v = y;
+    v.w = z;
+    return v;
+}
+
+Vec3 operator+(Vec3 lhs, const Vec3 &rhs) { return (lhs += rhs); }
+Vec3 operator-(Vec3 lhs, const Vec3 &rhs) { return (lhs -= rhs); }
+Vec3 operator*(Vec3 lhs, const Vec3 &rhs) { return (lhs *= rhs); }
+Vec3 operator/(Vec3 lhs, const Vec3 &rhs) { return (lhs /= rhs); }
+Vec3 operator*(Vec3 lhs, float rhs) { return (lhs *= rhs); }
 Vec3 operator/(Vec3 lhs, float rhs) { return (lhs /= rhs); }
 
 Vec3 operator+(const Vec3 &rhs)
@@ -98,7 +86,6 @@ Vec3 operator+(const Vec3 &rhs)
     vector.x = +rhs.x;
     vector.y = +rhs.y;
     vector.z = +rhs.z;
-    vector.w = +rhs.w;
     return vector;
 }
 
@@ -108,13 +95,12 @@ Vec3 operator-(const Vec3 &rhs)
     vector.x = -rhs.x;
     vector.y = -rhs.y;
     vector.z = -rhs.z;
-    vector.w = -rhs.w;
     return vector;
 }
 
 std::ostream &operator<<(std::ostream &os, const Vec3 &rhs)
 {
-    os << rhs.x << " " << rhs.y << " " << rhs.z << " " << rhs.w << std::endl;
+    os << "( " << rhs.x << " " << rhs.y << " " << rhs.z << " " << rhs.w << " )";
     return os;
 }
 
@@ -135,7 +121,8 @@ Vec3 cross(const Vec3 &lhs, const Vec3 &rhs)
 {
     return {lhs.y * rhs.z - lhs.z * rhs.y,
             lhs.z * rhs.x - lhs.x * rhs.z,
-            lhs.x * rhs.y - lhs.y * rhs.x};
+            lhs.x * rhs.y - lhs.y * rhs.x,
+            0};
 }
 
 Vec3 project(const Vec3 &line, const Vec3 &vector)
@@ -153,14 +140,59 @@ void orthonormal(Vec3 &normal, Vec3 &tangent)
     tangent = t;
 }
 
-std::optional<Vec3> intersect_plane(Vec3 point, Vec3 normal, Vec3 start, Vec3 end)
+std::optional<Vec3> intersect_plane(const Vec3 &point, const Vec3 &normal, const Vec3 &start, const Vec3 &end)
 {
     Vec3 n = normalize(normal);
-    Vec3 l = normalize(end - start);
-    float a = dot((point - start), n);
-    float b = dot(l, n);
-    if (a == 0 || b == 0)
+    Vec3 dir = end - start;
+    float a = dot(n, point);
+    float b = dot(n, dir);
+    if (b == 0) // The line is parallel with the plane
         return {};
-    float d = a / b;
-    return start + l * d;
+
+    float t = (a - dot(n, start)) / b;
+    if (t >= 0 && t <= 1) // The intersection is in the line segment
+        return start + dir * t;
+
+    return {};
 }
+
+Vec2 &Vec2::operator=(const Vec2 &other)
+{
+    if (this == &other)
+        return *this;
+    u = other.u;
+    v = other.v;
+    w = other.w;
+    return *this;
+}
+
+Vec2 &Vec2::operator+=(const Vec2 &rhs)
+{
+    u += rhs.u;
+    v += rhs.v;
+    w += rhs.w;
+    return *this;
+}
+
+Vec2 operator+(Vec2 lhs, const Vec2 &rhs) { return (lhs += rhs); }
+
+Vec2 &Vec2::operator-=(const Vec2 &rhs)
+{
+    u -= rhs.u;
+    v -= rhs.v;
+    w -= rhs.w;
+    return *this;
+}
+
+Vec2 operator-(Vec2 lhs, const Vec2 &rhs) { return (lhs -= rhs); }
+
+std::ostream &operator<<(std::ostream &os, const Vec2 &rhs)
+{
+    os << "( " << rhs.u << ", " << rhs.v << " " << rhs.w << " )";
+    return os;
+}
+
+float dot(const Vec2 &lhs, const Vec2 &rhs) { return lhs.u * rhs.u + lhs.v * rhs.v; }
+float abs_dot(const Vec2 &lhs, const Vec2 &rhs) { return std::abs(dot(lhs, rhs)); }
+float magnitude_squared(const Vec2 &vector) { return dot(vector, vector); }
+float magnitude(const Vec2 &vector) { return std::sqrt(magnitude_squared(vector)); }
--- library/vectors.hpp
+++ library/vectors.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "library.hpp"
+
 #include <cmath>
 #include <optional>
 #include <ostream>
@@ -10,22 +12,51 @@ struct Vec2
 
 	Vec2() : u(0), v(0), w(1) {}
 	Vec2(float u, float v) : u(u), v(v), w(1) {}
-	Vec2(float value) : u(value), v(value), w(1) {}
+	Vec2(float u, float v, float w) : u(u), v(v), w(w) {}
 	Vec2(const Vec2 &other) : u(other.u), v(other.v), w(1) {}
+
 	Vec2 &operator=(const Vec2 &);
+	Vec2 &operator+=(const Vec2 &);
+	Vec2 &operator-=(const Vec2 &);
+
+	friend std::ostream &operator<<(std::ostream &, const Vec2 &);
 };
 
+Vec2 operator+(Vec2 lhs, const Vec2 &rhs);
+Vec2 operator-(Vec2 lhs, const Vec2 &rhs);
+
+float dot(const Vec2 &lhs, const Vec2 &rhs);
+float abs_dot(const Vec2 &lhs, const Vec2 &rhs);
+float magnitude_squared(const Vec2 &vector);
+float magnitude(const Vec2 &vector);
+
+/**
+ * An implementation of 3-dimensional vectors and points.
+ */
 struct Vec3
 {
 	float x, y, z, w;
 
-	static const Vec3 RIGHT;
-	static const Vec3 UP;
-	static const Vec3 FORWARD;
-
+	/**
+	 * Default constructor.
+	 * Initializes a point at the origin.
+	 */
 	Vec3() : x(0), y(0), z(0), w(1) {}
+
+	/**
+	 * Constructs a point with the given components.
+	 */
 	Vec3(float x, float y, float z) : x(x), y(y), z(z), w(1) {}
-	Vec3(float value) : x(value), y(value), z(value), w(1) {}
+
+	/**
+	 * Constructs a vector with the given components.
+	 */
+	Vec3(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+	/**
+	 * Copy constructor.
+	 * Constructs a copy of the given vector.
+	 */
 	Vec3(const Vec3 &other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
 
 	Vec3 &operator=(const Vec3 &);
@@ -37,6 +68,12 @@ struct Vec3
 	Vec3 &operator/=(float);
 
 	friend std::ostream &operator<<(std::ostream &, const Vec3 &);
+	
+	static const Vec3 RIGHT;
+	static const Vec3 UP;
+	static const Vec3 FORWARD;
+
+	operator Vec2() const;
 };
 
 Vec3 operator+(Vec3 lhs, const Vec3 &rhs);
@@ -48,38 +85,60 @@ Vec3 operator/(Vec3 lhs, float rhs);
 Vec3 operator+(const Vec3 &);
 Vec3 operator-(const Vec3 &);
 
+/**
+ * Computes the dot product of two (2) vectors.
+ * @returns The dot product
+ */
 float dot(const Vec3 &lhs, const Vec3 &rhs);
+
+/**
+ * Computes the absolute value of the dot product of two (2) vectors.
+ * @returns The absolute value of the dot product
+ */
 float abs_dot(const Vec3 &lhs, const Vec3 &rhs);
+
+/**
+ * Computes the square of the magnitude of the given vector.
+ * @returns The square of the magnitude
+ */
 float magnitude_squared(const Vec3 &vector);
+
+/**
+ * Computes the magnitude of the given vector.
+ * @returns The magnitude
+ */
 float magnitude(const Vec3 &vector);
 
 /**
- * Normalizes a vector.
+ * Normalizes the given vector to be of length one (1).
  * Returns the zero vector if the original vector is very close to zero.
- * @return A unit vector with the same direction as the original vector.
- * @note Only considers the x, y, z components.
+ * @returns A unit vector with the same direction as the given vector
  */
 Vec3 normalize(const Vec3 &);
 
 /**
- * Computes the cross product of two vectors.
- * @return The cross product.
+ * Computes the cross product of two (2) vectors.
+ * @returns The cross product
  */
 Vec3 cross(const Vec3 &lhs, const Vec3 &rhs);
 
 /**
- * Projects the vector onto the given line.
+ * Projects the given vector onto the given line.
+ * @returns The projection
  */
 Vec3 project(const Vec3 &line, const Vec3 &vector);
 
 /**
- * Makes the normal orthogonal to the tangent.
- * @returns Two unit vectors that are orthonormal.
+ * Makes the normal vector orthogonal to the tangent vector.
  */
 void orthonormal(Vec3 &normal, Vec3 &tangent);
 
 /**
- * Computes the point where the line segment and plane intersect.
- * @return The point of intersection. If there is no intersection, returns nothing.
+ * Computes the intersection between the given line segment and plane.
+ * @param point A point on the plane
+ * @param normal A vector normal to the plane
+ * @param start The start of the line segment
+ * @param end The end of the line segment
+ * @returns Optionally the point of intersection
  */
-std::optional<Vec3> intersect_plane(Vec3, Vec3, Vec3, Vec3);
+std::optional<Vec3> intersect_plane(const Vec3 &point, const Vec3 &normal, const Vec3 &start, const Vec3 &end);
