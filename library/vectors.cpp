a2b76facd34bd90e8a18fedbf694409b05a2c8dd
130oclock <aodan13@att.net>
2025-03-03 17:44:21 -0500

    changed vector names

--- library/vectors.cpp
+++ library/vectors.cpp
@@ -1,50 +1,6 @@
 #include "vectors.hpp"
 #include "library.hpp"
 
-Vec2 &Vec2::operator=(const Vec2 &other)
-{
-    if (this == &other)
-        return *this;
-    u = other.u;
-    v = other.v;
-    w = other.w;
-    return *this;
-}
-
-Vec2 &Vec2::operator+=(const Vec2 &rhs)
-{
-    u += rhs.u;
-    v += rhs.v;
-    w += rhs.w;
-    return *this;
-}
-
-Vec2 operator+(Vec2 lhs, const Vec2 &rhs) { return (lhs += rhs); }
-
-Vec2 &Vec2::operator-=(const Vec2 &rhs)
-{
-    u -= rhs.u;
-    v -= rhs.v;
-    w -= rhs.w;
-    return *this;
-}
-
-Vec2 operator-(Vec2 lhs, const Vec2 &rhs) { return (lhs -= rhs); }
-
-std::ostream &operator<<(std::ostream &os, const Vec2 &rhs)
-{
-    os << "( " << rhs.u << " " << rhs.v << " " << rhs.w << " )";
-    return os;
-}
-
-float dot(const Vec2 &lhs, const Vec2 &rhs) { return lhs.u * rhs.u + lhs.v * rhs.v; }
-float abs_dot(const Vec2 &lhs, const Vec2 &rhs) { return std::abs(dot(lhs, rhs)); }
-float magnitude_squared(const Vec2 &input) { return dot(input, input); }
-float magnitude(const Vec2 &input) { return std::sqrt(magnitude_squared(input)); }
-const Vec3 Vec3::RIGHT = Vec3(1, 0, 0, 0);
-const Vec3 Vec3::UP = Vec3(0, 1, 0, 0);
-const Vec3 Vec3::FORWARD = Vec3(0, 0, 1, 0);
-
 Vec3 &Vec3::operator=(const Vec3 &other)
 {
     if (this == &other)
@@ -52,7 +8,6 @@ Vec3 &Vec3::operator=(const Vec3 &other)
     x = other.x;
     y = other.y;
     z = other.z;
-    w = other.w;
     return *this;
 }
 
@@ -104,15 +59,6 @@ Vec3 &Vec3::operator/=(float rhs)
     return *this;
 }
 
-Vec3::operator Vec2() const
-{
-    Vec2 v;
-    v.u = x;
-    v.v = y;
-    v.w = z;
-    return v;
-}
-
 Vec3 operator+(Vec3 lhs, const Vec3 &rhs) { return (lhs += rhs); }
 Vec3 operator-(Vec3 lhs, const Vec3 &rhs) { return (lhs -= rhs); }
 Vec3 operator*(Vec3 lhs, const Vec3 &rhs) { return (lhs *= rhs); }
@@ -140,20 +86,15 @@ Vec3 operator-(const Vec3 &rhs)
 
 std::ostream &operator<<(std::ostream &os, const Vec3 &rhs)
 {
-    os << "( " << rhs.x << " " << rhs.y << " " << rhs.z << " " << rhs.w << " )";
+    os << "( " << rhs.x << " " << rhs.y << " " << rhs.z << " )";
     return os;
 }
-
-float dot(const Vec3 &lhs, const Vec3 &rhs) { return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z; }
-float abs_dot(const Vec3 &lhs, const Vec3 &rhs) { return std::abs(dot(lhs, rhs)); }
-float magnitude_squared(const Vec3 &input) { return dot(input, input); }
-float magnitude(const Vec3 &input) { return std::sqrt(magnitude_squared(input)); }
 
 Vec3 normalize(const Vec3 &input)
 {
     float squared = magnitude_squared(input);
     if (almost_zero(squared))
-        return Vec3();
+        return Vec4();
     return input / std::sqrt(squared);
 }
 
@@ -161,31 +102,168 @@ Vec3 cross(const Vec3 &lhs, const Vec3 &rhs)
 {
     return {lhs.y * rhs.z - lhs.z * rhs.y,
             lhs.z * rhs.x - lhs.x * rhs.z,
+            lhs.x * rhs.y - lhs.y * rhs.x};
+}
+
+
+float dot(const Vec3 &lhs, const Vec3 &rhs) { return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z; }
+float abs_dot(const Vec3 &lhs, const Vec3 &rhs) { return std::abs(dot(lhs, rhs)); }
+float magnitude_squared(const Vec3 &input) { return dot(input, input); }
+float magnitude(const Vec3 &input) { return std::sqrt(magnitude_squared(input)); }
+
+const Vec4 Vec4::RIGHT = Vec4(1, 0, 0, 0);
+const Vec4 Vec4::UP = Vec4(0, 1, 0, 0);
+const Vec4 Vec4::FORWARD = Vec4(0, 0, 1, 0);
+
+Vec4 &Vec4::operator=(const Vec4 &other)
+{
+    if (this == &other)
+        return *this;
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    w = other.w;
+    return *this;
+}
+
+Vec4 &Vec4::operator+=(const Vec4 &rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    w += rhs.w;
+    return *this;
+}
+
+Vec4 &Vec4::operator-=(const Vec4 &rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    w -= rhs.w;
+    return *this;
+}
+
+Vec4 &Vec4::operator*=(const Vec4 &rhs)
+{
+    x *= rhs.x;
+    y *= rhs.y;
+    z *= rhs.z;
+    w *= rhs.w;
+    return *this;
+}
+
+Vec4 &Vec4::operator/=(const Vec4 &rhs)
+{
+    x /= rhs.x;
+    y /= rhs.y;
+    z /= rhs.z;
+    w /= rhs.w;
+    return *this;
+}
+
+Vec4 &Vec4::operator*=(float rhs)
+{
+    x *= rhs;
+    y *= rhs;
+    z *= rhs;
+    w *= rhs;
+    return *this;
+}
+
+Vec4 &Vec4::operator/=(float rhs)
+{
+    x /= rhs;
+    y /= rhs;
+    z /= rhs;
+    w /= rhs;
+    return *this;
+}
+
+Vec4::operator Vec3() const
+{
+    Vec3 v;
+    v.x = x;
+    v.y = y;
+    v.z = z;
+    return v;
+}
+
+Vec4 operator+(Vec4 lhs, const Vec4 &rhs) { return (lhs += rhs); }
+Vec4 operator-(Vec4 lhs, const Vec4 &rhs) { return (lhs -= rhs); }
+Vec4 operator*(Vec4 lhs, const Vec4 &rhs) { return (lhs *= rhs); }
+Vec4 operator/(Vec4 lhs, const Vec4 &rhs) { return (lhs /= rhs); }
+Vec4 operator*(Vec4 lhs, float rhs) { return (lhs *= rhs); }
+Vec4 operator/(Vec4 lhs, float rhs) { return (lhs /= rhs); }
+
+Vec4 operator+(const Vec4 &rhs)
+{
+    Vec4 vector;
+    vector.x = +rhs.x;
+    vector.y = +rhs.y;
+    vector.z = +rhs.z;
+    vector.w = +rhs.w;
+    return vector;
+}
+
+Vec4 operator-(const Vec4 &rhs)
+{
+    Vec4 vector;
+    vector.x = -rhs.x;
+    vector.y = -rhs.y;
+    vector.z = -rhs.z;
+    vector.w = -rhs.w;
+    return vector;
+}
+
+std::ostream &operator<<(std::ostream &os, const Vec4 &rhs)
+{
+    os << "( " << rhs.x << " " << rhs.y << " " << rhs.z << " " << rhs.w << " )";
+    return os;
+}
+
+float dot(const Vec4 &lhs, const Vec4 &rhs) { return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z + lhs.w * rhs.w; }
+float abs_dot(const Vec4 &lhs, const Vec4 &rhs) { return std::abs(dot(lhs, rhs)); }
+float magnitude_squared(const Vec4 &input) { return dot(input, input); }
+float magnitude(const Vec4 &input) { return std::sqrt(magnitude_squared(input)); }
+
+Vec4 normalize(const Vec4 &input)
+{
+    float squared = magnitude_squared(input);
+    if (almost_zero(squared))
+        return Vec4();
+    return input / std::sqrt(squared);
+}
+
+Vec4 cross(const Vec4 &lhs, const Vec4 &rhs)
+{
+    return {lhs.y * rhs.z - lhs.z * rhs.y,
+            lhs.z * rhs.x - lhs.x * rhs.z,
             lhs.x * rhs.y - lhs.y * rhs.x,
             0};
 }
 
-Vec3 project(const Vec3 &line, const Vec3 &vector)
+Vec4 project(const Vec4 &line, const Vec4 &vector)
 {
-    Vec3 n = normalize(line);
+    Vec4 n = normalize(line);
     float d = dot(n, vector);
     return n * d;
 }
 
-void orthonormal(Vec3 &normal, Vec3 &tangent)
+void orthonormal(Vec4 &normal, Vec4 &tangent)
 {
-    Vec3 n = project(normal, tangent);
-    Vec3 t = normalize(tangent - n);
+    Vec4 n = project(normal, tangent);
+    Vec4 t = normalize(tangent - n);
     normal = n;
     tangent = t;
 }
 
-std::optional<Vec3> intersect_plane(const Vec3 &point, const Vec3 &normal, const Vec3 &start, const Vec3 &end)
+std::optional<Vec4> intersect_plane(const Vec4 &point, const Vec4 &normal, const Vec4 &start, const Vec4 &end)
 {
-    Vec3 norm = normalize(normal);
-    Vec3 ray = end - start;
+    Vec4 norm = normalize(normal);
+    Vec4 ray = end - start;
 
-    Vec3 diff = start - point;
+    Vec4 diff = start - point;
     float a = dot(diff, norm);
     float b = dot(ray, norm);
     if (std::abs(b) < std::numeric_limits<float>::epsilon())
--- library/vectors.hpp
+++ library/vectors.hpp
@@ -6,98 +6,35 @@
 #include <optional>
 #include <ostream>
 
-struct Vec2
-{
-	float u, v, w;
-
-	/**
-	 * Default constructor.
-	 * Initializes a point at the origin.
-	 */
-	Vec2() : u(0), v(0), w(1) {}
-
-	/**
-	 * Constructs a point with the given components.
-	 */
-	Vec2(float u, float v) : u(u), v(v), w(1) {}
-
-	/**
-	 * Constructs a point with the given components.
-	 */
-	Vec2(uint32_t u, uint32_t v) : u((float)u), v((float)v), w(1) {}
-
-	/**
-	 * Constructs a point with the given components.
-	 */
-	Vec2(float u, float v, float w) : u(u), v(v), w(w) {}
-
-	/**
-	 * Copy constructor.
-	 */
-	Vec2(const Vec2 &other) : u(other.u), v(other.v), w(other.w) {}
-
-	Vec2 &operator=(const Vec2 &rhs);
-	Vec2 &operator+=(const Vec2 &rhs);
-	Vec2 &operator-=(const Vec2 &rhs);
-
-	friend std::ostream &operator<<(std::ostream &os, const Vec2 &rhs);
-};
-
-Vec2 operator+(Vec2 lhs, const Vec2 &rhs);
-Vec2 operator-(Vec2 lhs, const Vec2 &rhs);
-
-/**
- * Computes the dot product of two (2) vectors.
- * @returns The dot product
- */
-float dot(const Vec2 &lhs, const Vec2 &rhs);
-
-/**
- * Computes the absolute value of the dot product of two (2) vectors.
- * @returns The absolute value of the dot product
- */
-float abs_dot(const Vec2 &lhs, const Vec2 &rhs);
-
-/**
- * Computes the square of the magnitude of the given vector.
- * @returns The square of the magnitude
- */
-float magnitude_squared(const Vec2 &input);
-
-/**
- * Computes the magnitude of the given vector.
- * @returns The magnitude
- */
-float magnitude(const Vec2 &input);
-
-/**
- * An implementation of 3-dimensional vectors and points.
- */
 struct Vec3
 {
-	float x, y, z, w;
+	float x, y, z;
 
 	/**
 	 * Default constructor.
 	 * Initializes a point at the origin.
 	 */
-	Vec3() : x(0), y(0), z(0), w(1) {}
+	Vec3() : x(0), y(0), z(0) {}
 
 	/**
 	 * Constructs a point with the given components.
 	 */
-	Vec3(float x, float y, float z) : x(x), y(y), z(z), w(1) {}
+	Vec3(float u, float v) : x(u), y(v), z(0) {}
 
 	/**
-	 * Constructs a vector with the given components.
+	 * Constructs a point with the given components.
 	 */
-	Vec3(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+	Vec3(uint32_t u, uint32_t v) : x((float)u), y((float)v), z(0) {}
+
+	/**
+	 * Constructs a point with the given components.
+	 */
+	Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
 
 	/**
 	 * Copy constructor.
-	 * Constructs a copy of the given vector.
 	 */
-	Vec3(const Vec3 &other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
+	Vec3(const Vec3 &other) : x(other.x), y(other.y), z(other.z) {}
 
 	Vec3 &operator=(const Vec3 &rhs);
 	Vec3 &operator+=(const Vec3 &rhs);
@@ -108,12 +45,6 @@ struct Vec3
 	Vec3 &operator/=(float rhs);
 
 	friend std::ostream &operator<<(std::ostream &os, const Vec3 &rhs);
-
-	static const Vec3 RIGHT;   // The vector `<1, 0, 0>`
-	static const Vec3 UP;	   // The vector `<0, 1, 0>`
-	static const Vec3 FORWARD; // The vector `<0, 0, 1>`
-
-	operator Vec2() const;
 };
 
 Vec3 operator+(Vec3 lhs, const Vec3 &rhs);
@@ -163,15 +94,107 @@ Vec3 normalize(const Vec3 &input);
 Vec3 cross(const Vec3 &lhs, const Vec3 &rhs);
 
 /**
+ * An implementation of 3-dimensional vectors and points.
+ */
+struct Vec4
+{
+	float x, y, z, w;
+
+	/**
+	 * Default constructor.
+	 * Initializes a point at the origin.
+	 */
+	Vec4() : x(0), y(0), z(0), w(1) {}
+
+	/**
+	 * Constructs a point with the given components.
+	 */
+	Vec4(float x, float y, float z) : x(x), y(y), z(z), w(1) {}
+
+	/**
+	 * Constructs a vector with the given components.
+	 */
+	Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+	/**
+	 * Copy constructor.
+	 * Constructs a copy of the given vector.
+	 */
+	Vec4(const Vec4 &other) : x(other.x), y(other.y), z(other.z), w(other.w) {}
+
+	Vec4 &operator=(const Vec4 &rhs);
+	Vec4 &operator+=(const Vec4 &rhs);
+	Vec4 &operator-=(const Vec4 &rhs);
+	Vec4 &operator*=(const Vec4 &rhs);
+	Vec4 &operator/=(const Vec4 &rhs);
+	Vec4 &operator*=(float rhs);
+	Vec4 &operator/=(float rhs);
+
+	friend std::ostream &operator<<(std::ostream &os, const Vec4 &rhs);
+
+	static const Vec4 RIGHT;   // The vector `<1, 0, 0, 0>`
+	static const Vec4 UP;	   // The vector `<0, 1, 0, 0>`
+	static const Vec4 FORWARD; // The vector `<0, 0, 1, 0>`
+
+	operator Vec3() const;
+};
+
+Vec4 operator+(Vec4 lhs, const Vec4 &rhs);
+Vec4 operator-(Vec4 lhs, const Vec4 &rhs);
+Vec4 operator*(Vec4 lhs, const Vec4 &rhs);
+Vec4 operator/(Vec4 lhs, const Vec4 &rhs);
+Vec4 operator*(Vec4 lhs, float rhs);
+Vec4 operator/(Vec4 lhs, float rhs);
+Vec4 operator+(const Vec4 &rhs);
+Vec4 operator-(const Vec4 &rhs);
+
+/**
+ * Computes the dot product of two (2) vectors.
+ * @returns The dot product
+ */
+float dot(const Vec4 &lhs, const Vec4 &rhs);
+
+/**
+ * Computes the absolute value of the dot product of two (2) vectors.
+ * @returns The absolute value of the dot product
+ */
+float abs_dot(const Vec4 &lhs, const Vec4 &rhs);
+
+/**
+ * Computes the square of the magnitude of the given vector.
+ * @returns The square of the magnitude
+ */
+float magnitude_squared(const Vec4 &input);
+
+/**
+ * Computes the magnitude of the given vector.
+ * @returns The magnitude
+ */
+float magnitude(const Vec4 &input);
+
+/**
+ * Normalizes the given vector to be of length one (1).
+ * Returns the zero vector if the original vector is very close to zero.
+ * @returns A unit vector with the same direction as the given vector
+ */
+Vec4 normalize(const Vec4 &input);
+
+/**
+ * Computes the cross product of two (2) vectors.
+ * @returns The cross product
+ */
+Vec4 cross(const Vec4 &lhs, const Vec4 &rhs);
+
+/**
  * Projects the given vector onto the given line.
  * @returns The projection
  */
-Vec3 project(const Vec3 &line, const Vec3 &vector);
+Vec4 project(const Vec4 &line, const Vec4 &vector);
 
 /**
  * Makes the normal vector orthogonal to the tangent vector.
  */
-void orthonormal(Vec3 &normal, Vec3 &tangent);
+void orthonormal(Vec4 &normal, Vec4 &tangent);
 
 /**
  * Computes the intersection between the given line segment and plane.
@@ -181,4 +204,4 @@ void orthonormal(Vec3 &normal, Vec3 &tangent);
  * @param end The end of the line segment
  * @returns Optionally the point of intersection
  */
-std::optional<Vec3> intersect_plane(const Vec3 &point, const Vec3 &normal, const Vec3 &start, const Vec3 &end);
+std::optional<Vec4> intersect_plane(const Vec4 &point, const Vec4 &normal, const Vec4 &start, const Vec4 &end);
