022eafed16c72abadaaff26725cfb36b52707da9
130oclock <aodan13@att.net>
2026-02-04 16:21:48 -0500

    simplified checkpoint

--- main_checkpoint.cpp
+++ main_checkpoint.cpp
@@ -17,7 +17,7 @@ int main()
     Image image(ImageWidth, ImageHeight);
     DepthBuffer depth(ImageWidth, ImageHeight);
 
-    Matrix4 m_projection = perspective_projection(70, AspectRatio, 0.1, 100);
+    Matrix4 m_projection = perspective_projection(70, AspectRatio, 1, 100);
     Matrix4 m_screen = viewport(ImageWidth, ImageHeight);
 
     // Define our object
@@ -35,35 +35,10 @@ int main()
     for (size_t i = 0; i < mesh.vertex_size(); ++i)
     {
         vertices[i].world_coordinates = m_model * mesh.get_vertex(i);
-        vertices[i].world_normals = m_model * mesh.get_normal(i);
+        vertices[i].world_normals     = m_model * mesh.get_normal(i);
 
-        vertices[i].clip_coordinates = m_projection * vertices[i].world_coordinates;
-    }
-
-    std::vector<Triplet> triangles;
-
-    for (size_t i = 0; i < mesh.size(); ++i)
-    {
-        Triplet triangle = mesh[i];
-        std::vector<uint32_t> indices{triangle[0], triangle[1], triangle[2]};
-
-        // Clip triangles such that they are bounded within [-w, w] on all axes
-        sutherland_hodgman(indices, vertices);
-
-        if (indices.size() == 0)
-            continue;
-
-        // Reform triangles using fan triangulation
-        for (size_t j = 1; j < indices.size() - 1; ++j)
-        {
-            triangles.emplace_back(indices[0], indices[j], indices[j + 1]);
-        }
-    }
-
-    // Transform from clip space to screen space
-    for (size_t i = 0; i < vertices.size(); ++i)
-    {
         Vec4 &clip = vertices[i].clip_coordinates;
+        clip = m_projection * vertices[i].world_coordinates;
 
         // Scale by the depth
         float temp = clip.w;
@@ -78,9 +53,9 @@ int main()
     }
 
     // Draw each triangle
-    for (size_t i = 0; i < triangles.size(); ++i)
+    for (size_t i = 0; i < mesh.size(); ++i)
     {
-        Triplet triangle = triangles[i];
+        Triplet triangle = mesh[i];
         const VertexData::Vertex &v0 = vertices[triangle[0]], &v1 = vertices[triangle[1]], &v2 = vertices[triangle[2]];
 
         Color color(1);
